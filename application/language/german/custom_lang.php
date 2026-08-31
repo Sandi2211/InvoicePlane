d23be91b184b46d6ee129e9562d6860741e719b1
christoph <chris.sandbichler@gmail.com>
2026-08-31 22:29:04 +0200

    Add German language support for custom strings

---
+++ application/language/german/custom_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * CUSTOM LANGUAGE STRINGS (German).
+ *
+ * Extends the Crowdin-managed ip_lang.php without being overwritten
+ * by language pack updates. See application/language/english/custom_lang.php
+ * for details.
+ */
+$lang = [
+    'adults_from_15'                     => 'Erwachsene (ab 15 Jahren)',
+    'create_room_invoice'                => 'Zimmerrechnung erstellen',
+    'final_cleaning'                     => 'Endreinigung',
+    'final_cleaning_price_gross'         => 'Preis Endreinigung (Brutto)',
+    'price_per_night_gross'              => 'Preis pro Nacht (Brutto)',
+    'room'                               => 'Zimmer',
+    'room_invoice_default_group_missing' => 'Es ist kein Standard-Rechnungskreis festgelegt (Einstellungen).',
+    'room_invoice_family_missing'        => 'Keine Produkte in der Produktfamilie "Zimmer" gefunden.',
+    'room_invoice_product_missing'       => 'Erforderliches Produkt nicht gefunden:',
+];
